80f2e719e4d548efc5b70ebafb813faf10c5b959
Bekim LIka <bekim.lika2000@outlook.de>
2017-11-25 20:28:18 +0100

    Update 25.11.2017 20:27
    +Fixes an Issue with the Respawn of the Player
    +Added New LvL

---
+++ TheSquareGame/Assets/DeathLVL2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathLVL2 : MonoBehaviour {
+
+    public AudioSource SoundSource;
+    public AudioClip Sound;
+
+
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SoundSource.PlayOneShot(Sound);
+            other.transform.position = new Vector3(1000f, 17.25717f, -6.44f);
+            yield return new WaitForSeconds(1);
+            other.transform.position = new Vector3(4.152f, 1.004f, 3.929f);
+            other.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+
+        }
+
+
+    }
+}
