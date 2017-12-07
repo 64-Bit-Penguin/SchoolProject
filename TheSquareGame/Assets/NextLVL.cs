05319083c621bb92a559b229a90f571f18a92bbc
Bekim LIka <bekim.lika2000@outlook.de>
2017-12-07 09:56:44 +0100

    Update 07.12.2017
    +Improved Level System
       You have now LVL Bioms not each Lvl in one Scene
    +New Player Finish

--- TheSquareGame/Assets/NextLVL.cs
+++ TheSquareGame/Assets/NextLVL.cs
@@ -7,11 +7,24 @@ public class NextLVL : MonoBehaviour {
 
     [SerializeField] private string NextLevel;
 
+    public GameObject Level2;
+    public GameObject Level1;
+
+
+    void Start()
+    {
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(NextLevel);
+            Level2.SetActive(true);
+            Level1.SetActive(false);
+            other.transform.position = new Vector3(15.31271f, 1.35f, 2.742641f);
+            other.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+
         }
     }
 
