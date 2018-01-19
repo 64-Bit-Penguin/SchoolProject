9158a69e9f95427dfd2a658627221567e51515d2
Unknown <axel.stein1311@live.de>
2018-01-19 10:15:56 +0100

    Update 19.01.2018 10:15
    Added abillity to jump in all levels.

--- TheSquareGame/Assets/PlayerMovement.cs
+++ TheSquareGame/Assets/PlayerMovement.cs
@@ -5,18 +5,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float MoveSpeed;
+	public float JumpHeight;
+	public float CurrentLevel;
+	public bool JumpEnabled;
 
+	private Rigidbody rb;
+	private float Height;
+	private float MaxHeight;
 
 	// Use this for initialization
 	void Start () {
-        MoveSpeed = 2f;
+		rb = GetComponent <Rigidbody>();
+		MoveSpeed = 2f;
+		JumpHeight = 4f;
+		JumpEnabled = false;
+		CurrentLevel = 1;
+
+		if (CurrentLevel == 1f) {
+			MaxHeight = 0.5f;
+		} 
+		else if (CurrentLevel == 2f) {
+			MaxHeight = 1.29f;
+		}
+		else if (CurrentLevel == 3f) {
+			MaxHeight = 0.647f;
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-        transform.Translate(MoveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f, MoveSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+		transform.Translate(MoveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f, MoveSpeed*Input.GetAxis("Vertical") * Time.deltaTime);
+		Height = GameObject.Find ("Player").transform.position.y;
 
-
-}
+		if (Input.GetKeyDown (KeyCode.Space) && Height <= MaxHeight && JumpEnabled) {
+			rb.AddForce(new Vector3(0, JumpHeight, 0), ForceMode.Impulse);
+		}
+    }
 }
