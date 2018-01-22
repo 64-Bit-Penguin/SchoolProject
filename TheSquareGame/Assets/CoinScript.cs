429368611d64f2b50411f09ed74930b97e7b7770
Bekim LIka <bekim.lika2000@outlook.de>
2018-01-22 12:32:37 +0100

    Update 22.01.2018 12:29
    +Added Level 5
    +2 New scripts
       1. For Transport you in lvl4 to the other side
       2. You can now collect the coin .. Collect the coin
           to open the gate
    +Added Animation for the platform in level 4

---
+++ TheSquareGame/Assets/CoinScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+    public GameObject Coin;
+    public GameObject Wall;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Coin.SetActive(false);
+            Wall.SetActive(false);
+
+        }
+    }
+}
